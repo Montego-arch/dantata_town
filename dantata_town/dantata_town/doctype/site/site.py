0b7ef6d4c9f066bb1d94d17e284d04f99041e2cb
Montego-arch <mmanuelmiles@gmail.com>
2026-04-06 14:34:49 +0100

    fixing read only issues

--- dantata_town/dantata_town/doctype/site/site.py
+++ dantata_town/dantata_town/doctype/site/site.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2026, Montego-arch and contributors
 # For license information, please see license.txt
 
-# import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class Site(Document):
-	pass
+	def validate(self):
+		self.calculate_totals()
+
+	def calculate_totals(self):
+		total = 0
+		for row in self.get("project_units") or []:
+			row.amount = flt(row.projected_quantity) * flt(row.rate)
+			total += flt(row.projected_quantity)
+		self.total_units = total
