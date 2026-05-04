0e14fdbdefde0c81f47acd7ddf4e23f3c848b26a
Montego-arch <mmanuelmiles@gmail.com>
2026-05-04 11:48:53 +0100

    feat: filter Project building_type to chosen Site's project_units

--- dantata_town/public/js/project.js
+++ dantata_town/public/js/project.js
@@ -1,4 +1,16 @@
 frappe.ui.form.on("Project", {
+	setup(frm) {
+		frm.set_query("building_type", () => {
+			if (!frm.doc.site) {
+				return { filters: { name: ["in", []] } };
+			}
+			return {
+				query: "dantata_town.dantata_town.project_aggregations.get_site_building_types",
+				filters: { site: frm.doc.site },
+			};
+		});
+	},
+
 	refresh(frm) {
 		if (!frm.is_new() && frm.doc.site) {
 			frm.add_custom_button(__("Create BOQ"), () => {
@@ -8,6 +20,10 @@ frappe.ui.form.on("Project", {
 				});
 			});
 		}
+	},
+
+	site(frm) {
+		frm.set_value("building_type", null);
 	},
 
 	project_type(frm) {
